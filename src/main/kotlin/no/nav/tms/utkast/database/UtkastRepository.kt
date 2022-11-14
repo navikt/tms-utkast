5da31450d9ca04143a0c333f74a3e8593f25c5b8
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2022-11-14 17:23:29 +0100

    tmp: createUtkast

--- src/main/kotlin/no/nav/tms/utkast/database/UtkastRepository.kt
+++ src/main/kotlin/no/nav/tms/utkast/database/UtkastRepository.kt
@@ -1,10 +1,25 @@
 package no.nav.tms.utkast.database
 
 import com.zaxxer.hikari.HikariDataSource
+import kotliquery.queryOf
+import kotliquery.sessionOf
+import no.nav.helse.rapids_rivers.JsonMessage
+import org.postgresql.util.PGobject
+import java.time.LocalDateTime
 
-class UtkastRepository(val dataSource: HikariDataSource) {
+class UtkastRepository(private val dataSource: HikariDataSource) {
     fun createUtkast(packet: String) {
-        TODO("Not yet implemented")
+        sessionOf(dataSource).run(
+            queryOf(
+                "INSERT INTO utkast (packet, opprettet) values (:packet,:opprettet)",
+                mapOf("packet" to packet.jsonB(), "opprettet" to LocalDateTime.now())
+            ).asUpdate
+        )
     }
 
+}
+
+private fun String.jsonB() = PGobject().apply {
+    type = "jsonb"
+    value = this@jsonB
 }
