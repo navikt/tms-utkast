93364f962cc9c13fc39d6b6923b30b4095e07b4e
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2022-11-17 15:32:37 +0100

    producerlib: updated & created

--- lib/src/main/kotlin/no/nav/tms/utkast/producer/PacketBuilder.kt
+++ lib/src/main/kotlin/no/nav/tms/utkast/producer/PacketBuilder.kt
@@ -1,6 +1,8 @@
 package no.nav.tms.utkast.producer
 
 import no.nav.tms.utkast.producer.PacketBuilder.UtkastOperations.CREATED
+import no.nav.tms.utkast.producer.PacketBuilder.UtkastOperations.DELETED
+import no.nav.tms.utkast.producer.PacketBuilder.UtkastOperations.UPDATED
 import java.util.UUID
 
 object PacketBuilder {
@@ -9,11 +11,24 @@ object PacketBuilder {
         CREATED("created"), UPDATED("updated"), DELETED("deleted")
     }
 
-    fun created(eventId: UUID, tittel: String, link: String, ident: String) = mapOf<String, String>(
+    fun created(eventId: UUID, tittel: String, link: String, ident: String) = mapOf(
         "@event_name" to CREATED.eventName,
         "tittel" to tittel,
         "link" to link,
         "ident" to ident,
+        "eventId" to eventId.toString()
+    )
+
+    fun updated(eventId: UUID, tittel: String? = null, link: String? = null) = mutableMapOf(
+        "@event_name" to UPDATED.eventName,
+        "eventId" to eventId.toString()
+    ).apply {
+        if (!tittel.isNullOrEmpty()) { this["tittel"] = tittel }
+        if(!link.isNullOrEmpty()) this["link"] = link
+    }
+
+    fun deleted(eventId: UUID) = mutableMapOf(
+        "@event_name" to DELETED.eventName,
         "eventId" to eventId.toString()
     )
 }
--- lib/src/test/kotlin/no/nav/tms/utkast/producer/PacketBuilderTest.kt
+++ lib/src/test/kotlin/no/nav/tms/utkast/producer/PacketBuilderTest.kt
@@ -26,6 +26,52 @@ internal class PacketBuilderTest {
             this["tittel"] shouldBe testTittel
         }
     }
+
+    @Test
+    fun `bygger updated map`() {
+        val testId = UUID.randomUUID()
+        val testLink = "https://test.link"
+        val testTittel = "Bø på test"
+        PacketBuilder.updated(
+            eventId = testId,
+            link = testLink,
+            tittel = testTittel
+        ).assert {
+            this["@event_name"] shouldBe "updated"
+            UUID.fromString(this["eventId"]) shouldBe testId
+            this["link"] shouldBe testLink
+            this["tittel"] shouldBe testTittel
+        }
+
+        PacketBuilder.updated(
+            eventId = testId,
+            link = testLink,
+        ).assert {
+            this["@event_name"] shouldBe "updated"
+            UUID.fromString(this["eventId"]) shouldBe testId
+            this["link"] shouldBe testLink
+            this["tittel"] shouldBe null
+        }
+
+        PacketBuilder.updated(
+            eventId = testId,
+            tittel = testTittel,
+        ).assert {
+            this["@event_name"] shouldBe "updated"
+            UUID.fromString(this["eventId"]) shouldBe testId
+            this["link"] shouldBe null
+            this["tittel"] shouldBe testTittel
+        }
+    }
+
+    @Test
+    fun `bygger deleted map`(){
+        val testId = UUID.randomUUID()
+        PacketBuilder.deleted(testId).apply {
+            this["@event_name"] shouldBe "deleted"
+            UUID.fromString(this["eventId"]) shouldBe testId
+        }
+    }
 }
 
 internal inline fun <T> T.assert(block: T.() -> Unit): T =
