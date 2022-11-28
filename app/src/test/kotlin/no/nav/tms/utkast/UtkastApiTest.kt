d62f550088cb7fa94ba7293af69d82a84a1ac35d
Christopher Santana <christopher.santana@nav.no>
2022-11-28 10:12:37 +0100

    Henter tittel på ønsket språk dersom det finnes.

--- app/src/test/kotlin/no/nav/tms/utkast/UtkastApiTest.kt
+++ app/src/test/kotlin/no/nav/tms/utkast/UtkastApiTest.kt
@@ -27,78 +27,139 @@ class UtkastApiTest {
 
     private val utkastRepository = UtkastRepository(LocalPostgresDatabase.cleanDb())
     private val testRapid = TestRapid()
-    private val testFnr = "19873569100"
+    private val testFnr1 = "19873569100"
+    private val testFnr2 = "19873100100"
     private val startTestTime = LocalDateTimeHelper.nowAtUtc()
 
-    private val utkastForTestFnr = mutableListOf(
+    private val utkastForTestFnr1 = mutableListOf(
         testUtkastData(),
         testUtkastData(),
         testUtkastData(),
         testUtkastData()
     )
 
+    private val utkastForTestFnr2 = testUtkastData(tittelI18n = mapOf("en" to "English title", "nn" to "Nynorsk tittel"))
+
 
     @BeforeAll
     fun populate() {
         setupSinks(testRapid,utkastRepository)
-        utkastForTestFnr.forEach {
-            testRapid.sendTestMessage(it.toTestMessage())
+        utkastForTestFnr1.forEach {
+            testRapid.sendTestMessage(it.toTestMessage(testFnr1))
         }
-        testRapid.sendTestMessage(updateUtkastTestPacket(utkastForTestFnr[0].utkastId))
-        utkastForTestFnr[0] = utkastForTestFnr[0].copy(sistEndret = LocalDateTimeHelper.nowAtUtc())
+        testRapid.sendTestMessage(utkastForTestFnr2.toTestMessage(testFnr2))
+        testRapid.sendTestMessage(updateUtkastTestPacket(utkastForTestFnr1[0].utkastId))
+        utkastForTestFnr1[0] = utkastForTestFnr1[0].copy(sistEndret = LocalDateTimeHelper.nowAtUtc())
         testRapid.sendTestMessage(createUtkastTestPacket(utkastId = UUID.randomUUID().toString(), ident = "9988776655"))
         testRapid.sendTestMessage(createUtkastTestPacket(utkastId = UUID.randomUUID().toString(), ident = "9988776655"))
         testRapid.sendTestMessage(createUtkastTestPacket(utkastId = UUID.randomUUID().toString(), ident = "9988776655"))
     }
 
     @Test
-    fun `henter utkast for bruker med ident`() {
-        testApplication {
-            application { utkastApi(utkastRepository = utkastRepository, installAuthenticatorsFunction = {
-                installMockedAuthenticators {
-                    installTokenXAuthMock {
-                        alwaysAuthenticated = true
-                        setAsDefault = true
-                        staticUserPid = testFnr
-                        staticSecurityLevel = SecurityLevel.LEVEL_4
-                    }
+    fun `henter utkast for bruker med ident`() = testApplication {
+        application { utkastApi(utkastRepository = utkastRepository, installAuthenticatorsFunction = {
+            installMockedAuthenticators {
+                installTokenXAuthMock {
+                    alwaysAuthenticated = true
+                    setAsDefault = true
+                    staticUserPid = testFnr1
+                    staticSecurityLevel = SecurityLevel.LEVEL_4
                 }
-            } ) }
-
-            client.get("/utkast/antall").assert{
-                status shouldBe HttpStatusCode.OK
-                objectMapper.readTree(bodyAsText())["antall"].asInt() shouldBe 4
             }
-            client.get("/utkast").assert {
-                status.shouldBe(HttpStatusCode.OK)
-                objectMapper.readTree(bodyAsText()).assert {
-                    size() shouldBe 4
-                    forEach { jsonNode ->
-                        val utkastId = jsonNode["utkastId"].asText()
-                        val forventedeVerdier =
-                            utkastForTestFnr.find { it.utkastId == utkastId }
-                                ?: throw AssertionError("Fant utkast som ikke tilhører ident, utkastId : $utkastId")
-                        jsonNode["link"].asText() shouldBe forventedeVerdier.link
-                        jsonNode["opprettet"].asLocalDateTime() shouldNotBe null
-                        jsonNode["sistEndret"].asOptionalLocalDateTime() shouldBeCaSameAs forventedeVerdier.sistEndret
-                    }
+        } ) }
+
+        client.get("/utkast/antall").assert{
+            status shouldBe HttpStatusCode.OK
+            objectMapper.readTree(bodyAsText())["antall"].asInt() shouldBe 4
+        }
+
+        client.get("/utkast").assert {
+            status.shouldBe(HttpStatusCode.OK)
+            objectMapper.readTree(bodyAsText()).assert {
+                size() shouldBe 4
+                forEach { jsonNode ->
+                    val utkastId = jsonNode["utkastId"].asText()
+                    val forventedeVerdier =
+                        utkastForTestFnr1.find { it.utkastId == utkastId }
+                            ?: throw AssertionError("Fant utkast som ikke tilhører ident, utkastId : $utkastId")
+                    jsonNode["link"].asText() shouldBe forventedeVerdier.link
+                    jsonNode["opprettet"].asLocalDateTime() shouldNotBe null
+                    jsonNode["sistEndret"].asOptionalLocalDateTime() shouldBeCaSameAs forventedeVerdier.sistEndret
                 }
             }
         }
     }
 
-    private fun UtkastData.toTestMessage(ident: String = testFnr) = createUtkastTestPacket(
+    @Test
+    fun `forsøker å hente tittel på ønsket språk`() = testApplication {
+        application { utkastApi(utkastRepository = utkastRepository, installAuthenticatorsFunction = {
+            installMockedAuthenticators {
+                installTokenXAuthMock {
+                    alwaysAuthenticated = true
+                    setAsDefault = true
+                    staticUserPid = testFnr2
+                    staticSecurityLevel = SecurityLevel.LEVEL_4
+                }
+            }
+        } ) }
+
+        client.get("/utkast").assert {
+            status.shouldBe(HttpStatusCode.OK)
+            objectMapper.readTree(bodyAsText()).assert {
+                size() shouldBe 1
+
+                val tittel = get(0).get("tittel").textValue()
+
+                tittel shouldBe utkastForTestFnr2.tittel
+            }
+        }
+
+        client.get("/utkast?la=en").assert {
+            status.shouldBe(HttpStatusCode.OK)
+            objectMapper.readTree(bodyAsText()).assert {
+                size() shouldBe 1
+
+                val tittel = get(0).get("tittel").textValue()
+
+                tittel shouldBe utkastForTestFnr2.tittelI18n["en"]
+            }
+        }
+
+        client.get("/utkast?la=nn").assert {
+            status.shouldBe(HttpStatusCode.OK)
+            objectMapper.readTree(bodyAsText()).assert {
+                size() shouldBe 1
+
+                val tittel = get(0).get("tittel").textValue()
+
+                tittel shouldBe utkastForTestFnr2.tittelI18n["nn"]
+            }
+        }
+
+        client.get("/utkast?la=se").assert {
+            status.shouldBe(HttpStatusCode.OK)
+            objectMapper.readTree(bodyAsText()).assert {
+                size() shouldBe 1
+
+                val tittel = get(0).get("tittel").textValue()
+
+                tittel shouldBe utkastForTestFnr2.tittel
+            }
+        }
+    }
+
+    private fun UtkastData.toTestMessage(ident: String) = createUtkastTestPacket(
         utkastId = utkastId,
         ident = ident,
         link = link,
         tittel = tittel,
-
+        tittelI18n = tittelI18n
     )
 
-    private fun testUtkastData() = UtkastData(
+    private fun testUtkastData(tittelI18n: Map<String, String> = emptyMap()) = UtkastData(
         utkastId = UUID.randomUUID().toString(),
         tittel = "testTittel",
-        tittelI18n = mapOf("nb" to "Test tittel"),
+        tittelI18n = tittelI18n,
         link = "https://test.link",
         opprettet = startTestTime,
         sistEndret = null,
