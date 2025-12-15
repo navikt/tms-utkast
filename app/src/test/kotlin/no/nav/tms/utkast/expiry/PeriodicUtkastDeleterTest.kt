240f9d2f78c89844d02616528d2224baf3e12e98
Christopher Santana <christopher.santana@nav.no>
2025-12-15 13:10:49 +0100

    Tilpasser tidligere endring i dependencies.

--- app/src/test/kotlin/no/nav/tms/utkast/expiry/PeriodicUtkastDeleterTest.kt
+++ app/src/test/kotlin/no/nav/tms/utkast/expiry/PeriodicUtkastDeleterTest.kt
@@ -42,7 +42,7 @@ internal class PeriodicUtkastDeleterTest {
         .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
         .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
         .build()
-        .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+        .setDefaultPropertyInclusion(JsonInclude.Include.NON_NULL)
 
     @BeforeEach
     fun setup() {
